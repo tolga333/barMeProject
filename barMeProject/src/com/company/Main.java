1c0f294c5c52223ae9e4f7017c1f312b6e81fdd2
tolga333 <135798864+tolga333@users.noreply.github.com>
2023-07-16 12:50:15 +0300

    Add files via upload

--- barMeProject/src/com/company/Main.java
+++ barMeProject/src/com/company/Main.java
@@ -7,92 +7,7 @@ import java.util.*;
 
 public class Main {
 
-    // Метод за сортиране на ресторантите по разстояние от потребителя
-    private static void sortRestaurantsByDistance(String[][] restaurants, int userLocation) {
-        Arrays.sort(restaurants, Comparator.comparingInt(r -> Math.abs(Integer.parseInt(r[1]) - userLocation)));
-    }
-
-    // Метод за принтиране на данните за заведенията
-    private static void printRestaurantData(String[][] restaurants, int userLocation) {
-        System.out.println("Сортирани заведения:");
-        for (String[] restaurant : restaurants) {
-            int distance = Math.abs(Integer.parseInt(restaurant[1]) - userLocation);
-            System.out.println("Име: " + restaurant[0]);
-            System.out.println("Разстояние от вас: " + distance + "м");
-            System.out.println("Работно време: " + restaurant[2]);
-            System.out.println();
-        }
-    }
-    // Метод за извеждане на карта
-    private static void displayMap(String[][] restaurants, int userLocation) {
-        System.out.println("Списък с заведения (сортиран по разстояние):");
-        // Добавяме локацията на потребителя в списъка с заведенията
-        String[] user = {"Потребител(X)", String.valueOf(userLocation)};
-        String[][] allRestaurants = Arrays.copyOf(restaurants, restaurants.length + 1);
-        allRestaurants[restaurants.length] = user;
-
-        // Сортираме масива със заведения по разстояние във възходящ ред
-        Arrays.sort(allRestaurants, Comparator.comparingInt(o -> Integer.parseInt(o[1])));
-
-        // Извеждаме сортирания списък с заведенията
-        for (int i = 1; i < allRestaurants.length; i++) {
-            if(allRestaurants[i][0].equals("Потребител(X)")){
-                System.out.print( "X__");
-            }
-            System.out.print( i +"__");
-        }
-        System.out.println("");
-        for (int i = 0; i < allRestaurants.length; i++) {
-            System.out.println((i + 1) + ". " + allRestaurants[i][0]);
-        }
-
-
-        }
-        // Метод за сортиране по време на затваряне
-    private static void sortRestaurantsByClosingTime(String[][] restaurants) {
-        // Получаване на текущия час от часовника на компютъра
-        LocalTime currentTime = LocalTime.now();
-        int restorantindex=0;
-        // Създаване на компаратор за сравнение на ресторантите според разликата от текущия час
-        Comparator<String[]> closingTimeComparator = Comparator.comparing(restaurant -> {
-            String closingTime = restaurant[2];
-            String[] timeRange = closingTime.split(" - ");
-            LocalTime startTime = LocalTime.parse(timeRange[0]);
-            LocalTime endTime = LocalTime.parse(timeRange[1]);
-
-           //  Обработка случаи, когато затварянето е след полунощ
-            if (endTime.isBefore(startTime)) {
-                endTime = endTime.plusHours(24);
-            }
-
-            return Math.abs(endTime.toSecondOfDay() - currentTime.toSecondOfDay());
-        });
-
-        // Сортиране на заведенията по часа на затваряне
-        Arrays.sort(restaurants, closingTimeComparator);
-
-        // Извеждане на отворените заведения в момента
-        for (String[] restaurant : restaurants) {
-            String closingTime = restaurant[2];
-            String[] timeRange = closingTime.split(" - ");
-            LocalTime startTime = LocalTime.parse(timeRange[0]);
-            LocalTime endTime = LocalTime.parse(timeRange[1]);
-
-            // Обработка случаи, когато затварянето е след полунощ
-            if (endTime.isBefore(startTime)) {
-                endTime = endTime.plusHours(24);
-            }
-
-            if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
-                restorantindex++;
-                System.out.print(restorantindex+" ");
-                System.out.println("Заведение: " + restaurant[0]);
-                System.out.println("Работно време: " + closingTime);
-            }
-        }
-
-    }
-        // Метод за четене от файл
+    // Метод за четене от файл
     public static String[][] readDataFromFile(String filename) {
         try {
             File file = new File(filename);
@@ -119,12 +34,96 @@ public class Main {
                 restaurants[rowIndex] = restaurantData;
                 rowIndex++;
             }
-
             scanner.close();
             return restaurants;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    // Метод за сортиране на ресторантите по разстояние от потребителя
+    private static void sortRestaurantsByDistance(String[][] restaurants, int userLocation) {
+        Arrays.sort(restaurants, Comparator.comparingInt(r -> Math.abs(Integer.parseInt(r[1]) - userLocation)));
+    }
+
+    // Метод за принтиране на данните за заведенията
+    private static void printRestaurantData(String[][] restaurants, int userLocation) {
+        System.out.println("Сортирани заведения:");
+        for (String[] restaurant : restaurants) {
+            int distance = Math.abs(Integer.parseInt(restaurant[1]) - userLocation);
+            System.out.println("Име: " + restaurant[0]);
+            System.out.println("Разстояние от вас: " + distance + "м");
+            System.out.println("Работно време: " + restaurant[2]);
+            System.out.println();
+        }
+    }
+
+    // Метод за сортиране по време на затваряне
+    private static void sortRestaurantsByClosingTime(String[][] restaurants) {
+        // Получаване на текущия час от часовника на компютъра
+        LocalTime currentTime = LocalTime.now();
+        int restorantindex = 0;
+        // Създаване на компаратор за сравнение на ресторантите според разликата от текущия час
+        Comparator<String[]> closingTimeComparator = Comparator.comparing(restaurant -> {
+            String closingTime = restaurant[2];
+            String[] timeRange = closingTime.split(" - ");
+            LocalTime startTime = LocalTime.parse(timeRange[0]);
+            LocalTime endTime = LocalTime.parse(timeRange[1]);
+
+            //  Обработка случаи, когато затварянето е след полунощ
+            if (endTime.isBefore(startTime)) {
+                endTime = endTime.plusHours(24);
+            }
+
+            return Math.abs(endTime.toSecondOfDay() - currentTime.toSecondOfDay());
+        });
+
+        // Сортиране на заведенията по часа на затваряне
+        Arrays.sort(restaurants, closingTimeComparator);
+
+        // Извеждане на отворените заведения в момента
+        for (String[] restaurant : restaurants) {
+            String closingTime = restaurant[2];
+            String[] timeRange = closingTime.split(" - ");
+            LocalTime startTime = LocalTime.parse(timeRange[0]);
+            LocalTime endTime = LocalTime.parse(timeRange[1]);
+
+            // Обработка случаи, когато затварянето е след полунощ
+            if (endTime.isBefore(startTime)) {
+                endTime = endTime.plusHours(24);
+            }
+
+            if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
+                restorantindex++;
+                System.out.print(restorantindex + " ");
+                System.out.println("Заведение: " + restaurant[0]);
+                System.out.println("Работно време: " + closingTime);
+            }
+        }
+    }
+
+    // Метод за извеждане на карта
+    private static void displayMap(String[][] restaurants, int userLocation) {
+        System.out.println("Списък с заведения (сортиран по разстояние):");
+        // Добавяме локацията на потребителя в списъка с заведенията
+        String[] user = {"Потребител(X)", String.valueOf(userLocation)};
+        String[][] allRestaurants = Arrays.copyOf(restaurants, restaurants.length + 1);
+        allRestaurants[restaurants.length] = user;
+
+        // Сортираме масива със заведения по разстояние във възходящ ред
+        Arrays.sort(allRestaurants, Comparator.comparingInt(o -> Integer.parseInt(o[1])));
+
+        // Извеждаме сортирания списък с заведенията
+        for (int i = 1; i < allRestaurants.length; i++) {
+            if (allRestaurants[i][0].equals("Потребител(X)")) {
+                System.out.print("X__");
+            }
+            System.out.print(i + "__");
+        }
+        System.out.println("");
+        for (int i = 0; i < allRestaurants.length; i++) {
+            System.out.println((i + 1) + ". " + allRestaurants[i][0]);
         }
     }
 
@@ -148,7 +147,7 @@ public class Main {
         switch (option) {
             case 1:
                 // Сортиране на ресторантите според разстоянието от потребителя
-               sortRestaurantsByDistance(restaurants, userLocation);
+                sortRestaurantsByDistance(restaurants, userLocation);
                 // Извеждане на сортираните данни за заведенията
                 printRestaurantData(restaurants, userLocation);
                 break;
@@ -158,7 +157,7 @@ public class Main {
                 break;
             case 3:
                 // Извеждане на карта на потребителя
-                displayMap(restaurants,userLocation);
+                displayMap(restaurants, userLocation);
                 break;
             default:
                 System.out.println("Невалидна опция. Моля, изберете отново.");
